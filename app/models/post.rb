edea84783972786c0a92d92bbf3d67394b035016
cwilliams3579 <clevelandwilliams3579@gmail.com>
2017-02-01 17:00:57 -0500

    Add comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { in: 3..25 }
-  validates :body, presence: true, length: { in: 5..55 }
+  validates :body, presence: true, length: { minimum: 5 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   get 'post/new' => 'posts#new'
   get '/posts/:id/edit' => 'posts#edit'
   root to: 'posts#index'
